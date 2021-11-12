4d4452624cf068d166b47c9c76567e5429215a70
saracarolina12 <0226594@up.edu.mx>
2021-11-11 21:12:51 -0600

    '11/11/2021'

--- Semestres/Optimización y Metaheurísticas/Tabu/Busqueda_Tabu.R
+++ Semestres/Optimización y Metaheurísticas/Tabu/Busqueda_Tabu.R
@@ -1,17 +1,18 @@
 #Búsqueda Tabú
-profit =c(10,14,9,8,7,5,9,3)                  #beneficio
+profit =c(10,14,9,8,7,5,9,3)                  #Beneficio
 weights = c(7,12,8,9,8,6,11,5)                
 restr = 38                                    #j<=38
-sol = c(1,0,0,1,1,1,0,1)                      #solución inicial
-tabulist = c()                                #size = 2 
+sol = c(1,0,0,1,1,1,0,1)                      #Inicializar solución inicial
+tabulist = c()                                #Bloqueos 
 sol_prof = sum(sol*profit)
 #sol_wei = sum(weights*sol)
-for(i in 1:15){
-  
+
+for(i in 1:15){                               #Número de iteraciones
  #busca mejor solución
   benef_local = 0
  for(j in length(sol)){
-   sol[j] = 1 ^ sol[j]                        #XOR
+   sol[j] = 1 ^ sol[j]                        #XOR (forzar 1->0, 0->1)
+   print(sum(sol*profit))
    if(sum(sol*profit) > benef_local && sum(sol*weights)<=restr && !(j%in%tabulist)){
      benef_local = sum(sol*profit)
      indice = j
@@ -24,7 +25,7 @@ for(i in 1:15){
   sol_prof = max(c(sol_prof,benef_local))
 }
 
-print(sol_prof)
+# print(sol_prof)
 
 
 
