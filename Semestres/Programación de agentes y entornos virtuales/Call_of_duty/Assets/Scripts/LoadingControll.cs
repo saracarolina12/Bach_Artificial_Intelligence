21642edbb030a899abd8b3358955b8136823919f
sara_carolina12 <saracarogode@gmail.com>
2022-04-06 12:34:06 -0500

    'respaldo06abril2022'

--- Semestres/Programación de agentes y entornos virtuales/Call_of_duty/Assets/Scripts/LoadingControll.cs
+++ Semestres/Programación de agentes y entornos virtuales/Call_of_duty/Assets/Scripts/LoadingControll.cs
@@ -9,23 +9,36 @@ public class LoadingControll : MonoBehaviour
 {
     public GameObject loadingScreenObj;
     public Slider slider;
-    void Start(){
-        ExecuteAfterTime(1f);
-    }
-  
-     IEnumerator ExecuteAfterTime(float time)
+
+    //private float update;
+
+    void Awake()
     {
-        yield return new WaitForSeconds(time);
-    
-         for(;;){
-             Console.WriteLine(slider.value);
-            slider.value++;
-            if(slider.value == 0.9f){
-                slider.value = 1f;
-                break;
-            }
-            ExecuteAfterTime(2f);
-        }
+        Debug.Log("Awake");
+        slider.value = 0;
+        //update = 0.0f;
     }
+
+    void Start()
+    {
+        Debug.Log("Start1");
+        Debug.Log(slider.value);
+        StartCoroutine(ChangeProgressBar());
+    }
+    IEnumerator ChangeProgressBar(){
+        while(slider.value <= 100){
+            slider.value+= 5;
+            Debug.Log("before");
+            yield return new WaitForSeconds (.5f);
+            Debug.Log(slider.value);    
+            Debug.Log("after");
+            if(slider.value == 100)break;
+        }
+        yield return new WaitForSeconds (1.2f); 
+        Debug.Log("100% Change to main");
+        SceneManager.LoadScene("Main");   
+        Debug.Log(slider.value);  
+    }
+
 
 }
---
+++ Semestres/Programación de agentes y entornos virtuales/Call_of_duty/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerControllers control;
+    PlayerControllers.GameplayActions actions;
+    private Vector2 moveVector;
+    private Vector2 mouseVector;
+    private Rigidbody rb;
+	private Transform camT;
+    public float force = 10f;
+    public float sensibility = 0.1f;
+    private void Awake() { 
+        control = new PlayerControllers();
+        actions = control.Gameplay;
+        rb = GetComponent<Rigidbody>();
+        camT = transform.GetChild(0);
+		
+		actions.MoveCamera.performed += ctx => mouseVector = ctx.ReadValue<Vector2>();
+        actions.
+        Movement.performed += ctx => moveVector = ctx.ReadValue<Vector2>();
+    }
+    private void OnEnable() { 
+        control.Enable();
+    }
+    private void OnDestroy() { 
+        control.Disable();
+    }
+    private void Update() { 
+        transform.Rotate( 0f, mouseVector.x * sensibility ,0f);
+		camT.Rotate( -mouseVector.y * sensibility ,0f, 0f);	
+        rb.AddForce(((transform.forward * moveVector.y) + (transform.right * moveVector.x)) * force);
+    }
+}
