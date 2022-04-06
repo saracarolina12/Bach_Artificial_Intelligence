e137b7c8e367210be0e1a832d91a94903b5df720
sara_carolina12 <saracarogode@gmail.com>
2022-04-06 02:07:06 -0500

    'menu'

--- Semestres/Programación de agentes y entornos virtuales/Call_of_duty/Assets/Scripts/MainMenu.cs
+++ Semestres/Programación de agentes y entornos virtuales/Call_of_duty/Assets/Scripts/MainMenu.cs
@@ -6,6 +6,6 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void EscenaJuego(){
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("LoadingGame");
     }
 }
