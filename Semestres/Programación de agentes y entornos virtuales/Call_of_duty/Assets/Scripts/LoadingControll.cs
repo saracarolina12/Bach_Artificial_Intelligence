bbe493538ef23f197f5ee56ae38cc2ef7e234dd2
sara_carolina12 <saracarogode@gmail.com>
2022-04-06 08:00:48 -0500

    'respaldo06abril2022'

---
+++ Semestres/Programación de agentes y entornos virtuales/Call_of_duty/Assets/Scripts/LoadingControll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LoadingControll : MonoBehaviour
+{
+    public GameObject loadingScreenObj;
+    public Slider slider;
+    void Start(){
+        ExecuteAfterTime(1f);
+    }
+  
+     IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+    
+         for(;;){
+             Console.WriteLine(slider.value);
+            slider.value++;
+            if(slider.value == 0.9f){
+                slider.value = 1f;
+                break;
+            }
+            ExecuteAfterTime(2f);
+        }
+    }
+
+}
