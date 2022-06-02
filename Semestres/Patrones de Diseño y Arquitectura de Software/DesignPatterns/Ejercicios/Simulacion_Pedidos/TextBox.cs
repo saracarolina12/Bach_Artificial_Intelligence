4ca70196b4a20b6f36de494a170c82f046644b40
sara_carolina12 <saracarogode@gmail.com>
2022-06-01 21:10:03 -0500

    'final-updates'

---
+++ Semestres/Patrones de Diseño y Arquitectura de Software/DesignPatterns/Ejercicios/Simulacion_Pedidos/TextBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulacion_Pedidos
+{
+	class TextBox : Strategy
+	{
+		public override void AlgorithmInterface()
+		{
+			Console.WriteLine("Writing in TextBox");
+		}
+	}
+}
