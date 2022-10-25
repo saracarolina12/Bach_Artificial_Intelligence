0f267697b129c433325fc27f1557c81fa6014d29
sara_carolina12 <saracarogode@gmail.com>
2022-10-25 11:15:30 -0500

    25 Oct

--- Semestres/Extras/CM/PHP/Classes/SimpleClass.php
+++ Semestres/Extras/CM/PHP/Classes/SimpleClass.php
@@ -23,4 +23,16 @@
 
     $extended = new ExtendClass();
     $extended->displayVar();
+
+    // include files
+    // create functions
+    // open file as readable
+    // superglobal variable
+        // Which superglobal variable holds information about headers, paths, and script locations?
+    // add 1 to varialbe ++
+    // die and exit
+    //create cookie
+    // php array
+
 ?>
+
