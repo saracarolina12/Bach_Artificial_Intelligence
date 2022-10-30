6eb1a123e4bbc48cd8acf0ddf567a535fe408237
sara_carolina12 <saracarogode@gmail.com>
2022-10-30 09:25:49 -0600

    30 Oct

---
+++ Semestres/Optimización y Metaheurísticas II/Code/Metaheuristics/ParticleSwarmOptimization.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+class ParticleSwarmOptimization:
+    def __init__(self, nParticles, maxIter, func, bounds, args):
+        assert isinstance(nParticles, int), 'nParticles is not int'
+        assert isinstance(maxIter, int), 'maxIter is not int'
+        assert callable(func), 'func is not function'
+        bounds = np.array(bounds)
+        assert isinstance(bounds, np.ndarray), 'bounds is not np.ndarray'
+        args = np.array(args)
+        assert isinstance(args, np.ndarray), 'args is not np.ndarray'
+        self.nParticles = nParticles
+        self.maxIter = maxIter
+        self.func = func
+        self.bounds = bounds
+        self.args = args 
+        self.w,self.c1,self.c2 = 0.9,0.9,0.9
+    def _randomPositions(self):
+        pass
+    def _objectiveFunctionValues(self):
+        pass
+    def _getGBest(self):
+        pass
+    def updateVelocity(self):
+        pass
+    def updatePosition(self):
+        pass
+    def _solve(self):
+        # Initialize 𝑐1, 𝑐2, 𝑤
+        # Create the particles positions and velocities randomly
+        # Calculate the objective function values
+        # Calculate 𝑃𝑏𝑒𝑠𝑡
+        # 𝑖 as the current positions
+        # Calculate 𝐺𝑏𝑒𝑠𝑡
+        # While t < MaxIter or we haven’t found a good solution
+            # For each particle 𝑖
+                # Update the velocity:
+                # 𝑣 𝑖(𝑡 + 1) = 𝑤 𝑣 𝑖 (𝑡) + 𝑐1 𝑟1 ( 𝑃𝑏𝑒𝑠𝑡
+                # 𝑖 − 𝑥𝑖 (𝑡)) + 𝑐2 𝑟2 (𝐺𝑏𝑒𝑠𝑡 − 𝑥𝑖 (𝑡))
+                # Update the position:
+                # 𝑥𝑖 (𝑡 + 1) = 𝑥𝑖 (𝑡) + 𝑣 𝑖 (𝑡 + 1)
+                # Calculate 𝑓𝑢𝑛𝑐(𝑥𝑖 )
+                # If 𝑓(𝑥𝑖 ) < 𝑓𝑢𝑛𝑐(𝑃𝑏𝑒𝑠𝑡
+                # 𝑖 ): update 𝑃𝑏𝑒𝑠𝑡
+                # 𝑖
+                # If 𝑓(𝑥𝑖 ) < 𝑓𝑢𝑛𝑐(𝐺𝑏𝑒𝑠𝑡): update 𝐺𝑏𝑒𝑠𝑡
+        # Decrease 𝑐1, 𝑐2,𝑤
+        # return { }
+        pass
+def particle_swarm_optimization(nParticles, maxIter, func, bounds, args = ()):
+    pS = ParticleSwarmOptimization(nParticles, maxIter, func, bounds, args)
+    return pS.solve()
